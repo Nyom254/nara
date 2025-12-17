af12d7cae82aefedbcdbcf45ade8976e655f9d9b
yovan giofrey <yovangiofrey7@gmail.com>
2025-12-18 05:07:24 +0700

    integrate driver library

--- lwipopts.h
+++ lwipopts.h
@@ -89,4 +89,20 @@
 #define SLIP_DEBUG                  LWIP_DBG_OFF
 #define DHCP_DEBUG                  LWIP_DBG_OFF
 
+
+
+// Enable ALTCP and ALTCP TLS support
+#define LWIP_ALTCP 1
+
+// If you don't want to use TLS (just a http request) you can avoid linking to mbedtls and remove the following
+#define LWIP_ALTCP_TLS           1
+#define LWIP_ALTCP_TLS_MBEDTLS   1
+
+// Note bug in lwip with LWIP_ALTCP and LWIP_DEBUG
+// https://savannah.nongnu.org/bugs/index.php?62159
+//#define LWIP_DEBUG 1
+#undef LWIP_DEBUG
+#define ALTCP_MBEDTLS_DEBUG  LWIP_DBG_ON
+
+
 #endif 
--- main.c
+++ main.c
@@ -2,26 +2,74 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "lib/wireless_comm/wireless_comm.h"
+#include "lib/uart/sensor_comm/sensor_comm.h"
+#include "lib/spi/display_comm/display_comm.h"
+#include "lib/lvgl/lvgl.h"
+
 
 #define HIGH 1
 #define LOW 0
 
 
 
+static bool led_state = false;
+
+bool led_timer_cb(struct repeating_timer *t) {
+    led_state = !led_state;
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_state ? HIGH : LOW);
+    return true;
+}
+
+
+
 int main() {
   stdio_init_all();
-  if(cyw43_arch_init()){
+  while(cyw43_arch_init()){
     printf("failed to initialized\n");
+    sleep_ms(500);
   }
+  sleep_ms(1000);
   printf("initialized\n");
+
   
-  wifi_init();
+  // wifi_init();
+  // wifi_send_sensor_data("Example sensor data", 20);
+
+  // sensor_pin_init();
+  // sensor_data_t sensor_data;
+
+  ili9488_init();
+  spi_dma_init();
+  lv_init();
+  static struct repeating_timer lvgl_timer;
+  add_repeating_timer_ms(1, lvgl_tick_cb, NULL, &lvgl_timer);
+  lv_port_disp_init();
+
+  create_widgets();
+
+  static struct repeating_timer led_timer;
+  add_repeating_timer_ms(
+    500, 
+    led_timer_cb, 
+    NULL, 
+    &led_timer
+  );
+  
+
 
   while (true) {
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, HIGH);
-    sleep_ms(500);
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, LOW);
-    sleep_ms(500);
+    // scanf("Press Enter to read sensor data...\n");
+    // if (read_sensor_data(&sensor_data)) {
+    //   printf("Humidity: %.2f %%\n", sensor_data.humidity);
+    //   printf("Temperature: %.2f °C\n", sensor_data.temperature);
+    //   printf("Conductivity: %u µS/cm\n", sensor_data.conductivity);
+    //   printf("pH: %.2f\n", sensor_data.pH);
+    // } else {
+    //   printf("Failed to read sensor data\n");
+    // }
+
+    lv_timer_handler();
+    sleep_ms(5);
   }
 
   return 0;
