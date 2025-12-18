ec7f5790064dd301b22d62ec26e555284d9845fc
yovan giofrey <yovangiofrey7@gmail.com>
2025-12-18 23:46:52 +0700

    add header define

--- lib/spi/display_comm/display_comm.h
+++ lib/spi/display_comm/display_comm.h
@@ -1,5 +1,10 @@
+#ifndef DISPLAY_COMM_H
+#define DISPLAY_COMM_H
+
 void ili9488_init(void);
 void spi_dma_init(void);
 void lv_port_disp_init(void);
 void create_widgets(void);
 bool lvgl_tick_cb(struct repeating_timer *t);
+
+#endif
